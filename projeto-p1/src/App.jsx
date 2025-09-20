ec3083562042ba4299d86d11860b26d7448b15ae
Vinicius <oliveiravinicius010@gmail.com>
2025-09-20 13:32:59 -0300

    feat(p1): cria componente principal e faz hello

---
+++ projeto-p1/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="container mt-2">
+      <div className="row">
+        <div className="col-12 col-md-8 text-center">
+           <p> Hello, Lembretes </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
